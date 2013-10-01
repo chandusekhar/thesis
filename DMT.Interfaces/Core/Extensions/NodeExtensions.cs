9734ef226554477724e2b2a01d60c613d7db3a57
Tamas Michelberger <tomi.michel@gmail.com>
2013-10-02 00:46:08 +0200

    NodeExtensions added
    method to access all edges in a node

---
+++ DMT.Interfaces/Core/Extensions/NodeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Interfaces.Core.Extensions
+{
+    public static class NodeExtensions
+    {
+        /// <summary>
+        /// Union of inbound and outbound edges.
+        /// </summary>
+        /// <returns>An enumerable containing the inbound and outbound edges of the node.</returns>
+        public static IEnumerable<IEdge> AllEdges(this INode self)
+        {
+            return self.InboundEdges.Union(self.OutboundEdges);
+        }
+    }
+}
