30af73722b28b6cfaee1c49bb8e5b3f88647060e
Tamas Michelberger <tomi.michel@gmail.com>
2013-10-18 14:08:42 +0200

    Added logging to CoreEntityFactory.

--- DMT.Core/Serialization/CoreEntityFactory.cs
+++ DMT.Core/Serialization/CoreEntityFactory.cs
@@ -4,28 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DMT.Core.Interfaces.Serialization;
+using NLog;
 
 namespace DMT.Core.Serialization
 {
     public class CoreEntityFactory : IEntityFactory
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public Interfaces.IId CreateId()
         {
+            logger.Trace("Created new id.");
             return Id.NewId();
         }
 
         public Interfaces.INode CreateNode()
         {
+            logger.Trace("Created new node.");
             return new Node();
         }
 
         public Interfaces.IEdge CreateEdge()
         {
+            logger.Trace("Created new edge.");
             return new Edge();
         }
 
         public Interfaces.IEdge CreateEdge(Interfaces.INode start, Interfaces.INode end)
         {
+            logger.Trace("Created new edge.");
             return new Edge(start, end);
         }
     }
