42479a0f410679142ce9e1b55b7cdae5cb7c0997
Tamas Michelberger <tomi.michel@gmail.com>
2013-10-18 13:25:46 +0200

    IDataSrource as an entry point for loading and saving models

---
+++ DMT.Core.Interfaces/Serialization/IDataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Core.Interfaces.Serialization
+{
+    /// <summary>
+    /// Interface for loading nodes from raw source.
+    /// </summary>
+    public interface IDataSource : IDisposable
+    {
+        /// <summary>
+        /// Loads the model.
+        /// </summary>
+        /// <returns<>The root nodes of the graph components as a collection./returns>
+        Task<IEnumerable<INode>> LoadModelAsync();
+
+        /// <summary>
+        /// Saves the model.
+        /// </summary>
+        Task SaveModelAsync();
+    }
+}
