17a2d2d54248975f68f4e48276fd97d7de1be3bd
Tamas Michelberger <tomi.michel@gmail.com>
2013-10-18 12:49:39 +0200

    Created IEntityFactory and basic implementation.

---
+++ DMT.Core.Interfaces/Serialization/IEntityFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Core.Interfaces.Serialization
+{
+    /// <summary>
+    /// Interface for entity creation.
+    /// </summary>
+    public interface IEntityFactory
+    {
+        /// <summary>
+        /// Create an id that implements the <c>IId</c> interface.
+        /// </summary>
+        /// <returns>A new id.</returns>
+        IId CreateId();
+    }
+}
---
+++ DMT.Core/Serialization/CoreEntityFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMT.Core.Interfaces.Serialization;
+
+namespace DMT.Core.Serialization
+{
+    public class CoreEntityFactory : IEntityFactory
+    {
+        public Interfaces.IId CreateId()
+        {
+            return Id.NewId();
+        }
+    }
+}
