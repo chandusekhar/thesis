0623f93ecf55c1831dbd196f4dc884a019a17615
Tamas Michelberger <tomi.michel@gmail.com>
2013-10-26 21:33:38 +0200

    InjectableBase implements IPartImportsSatisfiedNotification

--- DMT.Common/Composition/InjectableBase.cs
+++ DMT.Common/Composition/InjectableBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace DMT.Common.Composition
     /// Base for classes that has satisfiable imports.
     /// It satisfies the imports automatically on instatiation in dev and production environments.
     /// </summary>
-    public abstract class InjectableBase
+    public abstract class InjectableBase : IPartImportsSatisfiedNotification
     {
         public InjectableBase()
         {
@@ -19,5 +20,10 @@ namespace DMT.Common.Composition
                 CompositionService.Instance.Inject(this);
             }
         }
+
+        /// <summary>
+        /// This is called, when all imports that could be satisfied have been satisfied.
+        /// </summary>
+        public virtual void OnImportsSatisfied() { }
     }
 }
