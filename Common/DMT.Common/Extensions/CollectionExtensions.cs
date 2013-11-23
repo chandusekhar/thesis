8eb3e64abcc26b55c23e73b51a2263501f00f954
Tamas Michelberger <tomi.michel@gmail.com>
2013-11-23 16:09:35 +0100

    added Common.Extensions.CollectionExtensions#AddAll

---
+++ Common/DMT.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Common.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void AddAll<T>(this ICollection<T> self, IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                self.Add(item);
+            }
+        }
+    }
+}
