bd4b410a1563e3a99cfbf8b2d15d27e72529fe91
Tamas Michelberger <tomi.michel@gmail.com>
2014-02-07 13:21:06 +0100

    maximal matching coarsening

---
+++ Common/DMT.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
+        {
+            if (self == null)
+            {
+                return;
+            }
+
+            foreach (var item in self)
+            {
+                action(item);
+            }
+        }
+    }
+}
