11c9abeeb9a90a0b8795ebc4d1e92483b4caebc5
Tamas Michelberger <tomi.michel@gmail.com>
2014-03-11 15:03:16 +0100

    find matcher client

--- Applications/DMT.Matcher.Module/Partitioner/PartitionBrokerServiceClient.cs
+++ Applications/DMT.Matcher.Module/Partitioner/PartitionBrokerServiceClient.cs
@@ -7,19 +7,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using DMT.Common.Rest;
+using DMT.Core.Interfaces;
 using DMT.Module.Common.Service;
 
 namespace DMT.Matcher.Module.Partitioner
 {
     class PartitionBrokerServiceClient
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         public readonly string BaseAddress;
 
         public const string MatchersPath = "/matchers";
 
         public PartitionBrokerServiceClient(string baseAddress)
         {
-            this.BaseAddress = baseAddress;
+            this.BaseAddress = baseAddress.TrimEnd('/');
         }
 
         public PartitionBrokerServiceClient(Uri baseAddress)
@@ -53,11 +56,35 @@ namespace DMT.Matcher.Module.Partitioner
 
         public void MarkMatcherReady(Guid id)
         {
-            string url = string.Format("{0}{1}/{2}/ready", this.BaseAddress.TrimEnd('/'), MatchersPath, id);
+            string url = string.Format("{0}{1}/{2}/ready", this.BaseAddress, MatchersPath, id);
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
             req.Method = HttpMethod.Put;
             req.ContentLength = 0;
             using (req.GetResponse()) ;
+        }
+
+        public MatcherInfo FindMatcher(IId partitionId)
+        {
+            MatcherInfoResponse response = null;
+
+            string path = string.Format("{0}/find/{1}", MatchersPath, partitionId.ToUrlSafe());
+            using (WebClient wc = new WebClient { BaseAddress = this.BaseAddress })
+            using (var responseStream = wc.OpenRead(path))
+            {
+                var s = new XmlSerializer(typeof(MatcherInfoResponse));
+                response = (MatcherInfoResponse)s.Deserialize(responseStream);
+            }
+
+            if (response.Success)
+            {
+                return response.Result;
+            }
+            else
+            {
+                logger.Error("Error while getting matcher address: {0}.", response.ErrorMessage);
+                // TODO: throw an exception?
+                return null;
+            }
         }
     }
 }
