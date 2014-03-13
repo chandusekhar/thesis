1babda777a0e6d14a4760550632f65f35d5eb7c7
Tamas Michelberger <tomi.michel@gmail.com>
2014-03-13 11:41:40 +0100

    added local matcher job

---
+++ Jobs/DMT.Matcher.Local/LocalMatcherJob.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMT.Matcher.Interfaces;
+
+namespace DMT.Matcher.Local
+{
+    class LocalMatcherJob : IMatcherJob
+    {
+        public string Name
+        {
+            get { return "Local matcher job"; }
+        }
+
+        public event MatcherJobDoneEventHandler Done;
+
+        public void Initialize(IMatcherFramework framework)
+        {
+            Console.WriteLine("Hello from local matcher!");
+        }
+
+        public void Start(MatchMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> FindPartialMatch(Core.Interfaces.IId paritionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jobs/DMT.Matcher.Local/README.md
@@ -0,0 +1,5 @@
+﻿DMT.Matcher.Local
+=================
+
+Basic implementation of a matcher job. It searches matches only in
+the local parition - never reaches to remote partitions.
