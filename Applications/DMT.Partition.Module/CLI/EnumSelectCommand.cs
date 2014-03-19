fe12d1245a9e7d4b744fa6ff8cf3f160b70213a1
Tamas Michelberger <tomi.michel@gmail.com>
2014-03-19 14:40:12 +0100

    enum select command for the console

---
+++ Applications/DMT.Partition.Module/CLI/EnumSelectCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Partition.Module.CLI
+{
+    class EnumSelectCommand<T> : CommandBase
+    {
+        public T Result { get; private set; }
+
+        public EnumSelectCommand(char code, string desc)
+            : base(code, desc)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enum");
+            }
+        }
+
+        public override void Execute()
+        {
+            bool ok = false;
+            string[] names = Enum.GetNames(typeof(T));
+
+            while (!ok)
+            {
+                Console.WriteLine("Select one:");
+                for (int i = 0; i < names.Length; ++i)
+                {
+                    Console.WriteLine("{0} - {1}", i, names[i]);
+                }
+
+                var selected = Console.ReadLine();
+                int index;
+                if (ok = int.TryParse(selected, out index))
+                {
+                    this.Result = (T)Enum.Parse(typeof(T), names[index]);
+                    break;
+                }
+            }
+        }
+    }
+}
