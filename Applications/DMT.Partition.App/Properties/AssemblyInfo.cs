58ccd72a9e4b4b9c17d1ec3b8568aa3288db31be
Tamas Michelberger <tomi.michel@gmail.com>
2014-03-30 21:50:28 +0200

    rename assembly: DMT.Partition.App

--- Applications/DMT.Partition.App/Properties/AssemblyInfo.cs
+++ Applications/DMT.Partition.App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DMT.Master.App")]
+[assembly: AssemblyTitle("DMT.Partition.App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DMT.Master.App")]
+[assembly: AssemblyProduct("DMT.Partition.App")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
